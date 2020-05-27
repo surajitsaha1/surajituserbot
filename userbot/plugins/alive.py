36420b034a9a0ceef0f735e65cc014dda7e918f4
surajitsaha1 <65535432+surajitsaha1@users.noreply.github.com>
2020-05-28 00:15:25 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,7 +6,7 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Guide.](https://how2techy.com/xtra-guide1/)"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "**No Name set yet.** [Check Guide.](https://t.me/surajit1/)"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
@@ -14,6 +14,6 @@ async def amireallyalive(alive):
     await alive.edit("`Currently Alive, my peru master!` **ψ(｀∇´)ψ**\n\n"
                      "`Telethon version: 6.9.0\nPython: 3.7.3\n`"
                      # Don't change this else you a TikTok loser, Son of Jinping. Add your own.
-                     "`Bot created by:` [SnapDragon](tg://user?id=719877937), @anubisxx\n"
+                     "`Bot created by:` Surajit Saha. @surajit1\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "https://github.com/Dark-Princ3/X-tra-Telegram")
+                     "https://t.me/surajit1")
